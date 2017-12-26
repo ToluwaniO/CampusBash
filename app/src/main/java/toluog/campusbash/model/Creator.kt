ae54acd63640f9b366eacd8a2a82794ce2196e71
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-12-25 23:57:09 -0500

    removed parcelable boilerplate code and replaced it with kotlinx annotation

--- app/src/main/java/toluog/campusbash/model/Creator.kt
+++ app/src/main/java/toluog/campusbash/model/Creator.kt
@@ -1,31 +1,13 @@
 package toluog.campusbash.model
 
+import android.annotation.SuppressLint
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by oguns on 12/2/2017.
  */
-data class Creator(var name: String = "", var imageUrl: String = "", var uid: String = "") : Parcelable {
-    constructor(source: Parcel) : this(
-            source.readString(),
-            source.readString(),
-            source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(name)
-        writeString(imageUrl)
-        writeString(uid)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<Creator> = object : Parcelable.Creator<Creator> {
-            override fun createFromParcel(source: Parcel): Creator = Creator(source)
-            override fun newArray(size: Int): Array<Creator?> = arrayOfNulls(size)
-        }
-    }
-}
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class Creator(var name: String = "", var imageUrl: String = "", var uid: String = "") : Parcelable
