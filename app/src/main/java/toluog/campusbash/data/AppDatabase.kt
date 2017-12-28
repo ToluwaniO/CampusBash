ca0169ac920c61d9d6f1ac516db00b7ebbfbc1ee
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-12-28 12:11:19 -0500

    unit tests for event table and university table

--- app/src/main/java/toluog/campusbash/data/AppDatabase.kt
+++ app/src/main/java/toluog/campusbash/data/AppDatabase.kt
@@ -3,15 +3,17 @@ package toluog.campusbash.data
 import android.arch.persistence.room.*
 import android.content.Context
 import toluog.campusbash.model.Event
+import toluog.campusbash.model.University
 
 /**
  * Created by oguns on 12/4/2017.
  */
-@Database(entities = arrayOf(Event::class), version = 1)
+@Database(entities = arrayOf(Event::class, University::class), version = 1)
 @TypeConverters(EventConverter::class)
 abstract class AppDatabase(): RoomDatabase() {
 
     abstract fun eventDao() : EventDao
+    abstract fun universityDao(): UniversityDao
 
     companion object {
         var dbInstance: AppDatabase? = null
---
+++ app/src/main/java/toluog/campusbash/data/UniversityDao.kt
@@ -0,0 +1,40 @@
+package toluog.campusbash.data
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.*
+import toluog.campusbash.model.University
+import toluog.campusbash.utils.AppContract
+
+/**
+ * Created by oguns on 12/28/2017.
+ */
+@Dao
+public interface UniversityDao{
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUniversity(university: University)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUniversities(universities: List<University>)
+
+    @Update
+    fun updateUniversity(university: University)
+
+    @Update
+    fun updateUniversities(universities: List<University>)
+
+    @Query("SELECT * FROM ${AppContract.UNIVERSITY_TABLE} WHERE uniId LIKE :id LIMIT 1")
+    fun getUniversity(id: String): LiveData<University>
+
+    @Query("SELECT * FROM ${AppContract.UNIVERSITY_TABLE} WHERE country LIKE :country")
+    fun getUniversities(country: String): LiveData<List<University>>
+
+    @Query("SELECT * FROM ${AppContract.UNIVERSITY_TABLE}")
+    fun getUniversities(): LiveData<List<University>>
+
+    @Delete
+    fun deleteUniversity(university: University)
+
+    @Delete
+    fun deleteUniversities(universities: List<University>)
+}
