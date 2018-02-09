5bccac9a47db5a460afd60cdecfa8be19c100328
ToluwaniO <ToluwaniO@users.noreply.github.com>
2018-02-09 00:04:01 -0500

    # Fixed fab behavior (#33)

---
+++ app/src/main/java/toluog/campusbash/ViewBehavior/FabScrollBehavior.kt
@@ -0,0 +1,37 @@
+package toluog.campusbash.ViewBehavior
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by oguns on 2/8/2018.
+ */
+class FabScrollBehavior: FloatingActionButton.Behavior {
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+
+    }
+    constructor():super()
+
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
+                override fun onHidden(fab: FloatingActionButton?) {
+                    super.onHidden(fab)
+                    fab?.visibility = View.INVISIBLE
+                }
+            })
+        } else if (dyConsumed < 0 && child.visibility == View.INVISIBLE) {
+            child.show()
+        }
+    }
+
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, directTargetChild: View, target: View, nestedScrollAxes: Int): Boolean {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+}
