c65d55dfa4a7c1260e4eec32c49ae0535f510aa9
ToluwaniO <ogunsanyatoluwani@gmail.com>
2018-06-14 18:20:39 -0400

    date, strings.xml values

--- app/src/main/java/toluog/campusbash/adapters/BoughtTicketAdapter.kt
+++ app/src/main/java/toluog/campusbash/adapters/BoughtTicketAdapter.kt
@@ -9,7 +9,9 @@ import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.bought_ticket_layout.*
 import toluog.campusbash.R
 import toluog.campusbash.model.BoughtTicket
+import toluog.campusbash.utils.Util
 import toluog.campusbash.utils.loadImage
+import java.util.*
 
 class BoughtTicketAdapter(var tickets: List<BoughtTicket>, var context: Context?): RecyclerView.Adapter<BoughtTicketAdapter.ViewHolder>() {
 
@@ -28,21 +30,24 @@ class BoughtTicketAdapter(var tickets: List<BoughtTicket>, var context: Context?
     override fun getItemCount() = tickets.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(tickets[position], listener)
+        holder.bind(tickets[position], listener, context)
     }
 
-
     class ViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
-        fun bind(ticket: BoughtTicket, listener: TicketListener) {
+        fun bind(ticket: BoughtTicket, listener: TicketListener, context: Context?) {
             if(ticket.ticketCodes.isNotEmpty()) {
                 event_ticket.loadImage(ticket.placeholderImage.url)
             }
             event_name.text = ticket.eventName
             tickets_number.text = if(ticket.ticketCodes.size > 1) {
-                "${ticket.ticketCodes.size} tickets"
+                context?.getString(R.string.ticket_quantity_with_params, ticket.ticketCodes.size)
             } else {
-                "${ticket.ticketCodes.size} ticket"
+                context?.getString(R.string.ticket_quantity_one)
             }
+            val time = Util.formatDate(Calendar.getInstance().apply {
+                timeInMillis = ticket.eventTime
+            })
+            bought_date.text = context?.getString(R.string.starts_on_x, time)
 
             containerView.setOnClickListener {
                 listener.onTicketClicked(ticket)
