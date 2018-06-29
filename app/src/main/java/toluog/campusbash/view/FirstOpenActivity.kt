94cedc21e5e4b808cbb2870d25659decdb8ef741
ToluwaniO <ToluwaniO@users.noreply.github.com>
2018-06-29 00:30:27 -0400

    transition change (#89)

--- app/src/main/java/toluog/campusbash/view/FirstOpenActivity.kt
+++ app/src/main/java/toluog/campusbash/view/FirstOpenActivity.kt
@@ -1,6 +1,8 @@
 package toluog.campusbash.view
 
 import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -15,30 +17,52 @@ import toluog.campusbash.utils.FirebaseManager
 import toluog.campusbash.utils.Util
 import com.firebase.ui.auth.ErrorCodes.NO_NETWORK
 import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseUser
 import toluog.campusbash.utils.AppContract.Companion.RC_SIGN_IN
 
 
 class FirstOpenActivity : AppCompatActivity(), PickUniversityFragment.PickUniversityListener,
 PickEventTypeFragment.PickEventTypeListener{
 
-    val fragManager = supportFragmentManager
-    var count = 0
+    private val fragManager = supportFragmentManager
+    private lateinit var viewModel: GeneralViewModel
+    private val fbManager = FirebaseManager()
+    private var user: FirebaseUser? = null
+    private var count = 0
     private val TAG = FirstOpenActivity::class.java.simpleName
+
+    private var country: String? = null
+    private var university: String? = null
+    private var prefs: List<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_open)
         Log.d(TAG, "Activity opened")
+        user = FirebaseManager.getUser()
+        viewModel = ViewModelProviders.of(this).get(GeneralViewModel::class.java)
     }
 
     override fun universitySelectionDone(country: String, name: String) {
         Util.setPrefString(act, AppContract.PREF_UNIVERSITY_KEY, name)
         Util.setPrefString(act, AppContract.PREF_COUNTRY_KEY, country)
-        count++
-        fragManager.beginTransaction().replace(R.id.fragment_frame, PickEventTypeFragment(), null).commit()
+        user?.let {
+            fbManager.updateProfileField(AppContract.FIREBASE_USER_UNIVERSITY, name, it)
+            fbManager.updateProfileField(AppContract.FIREBASE_USER_COUNTRY, country, it)
+        }
+        if(prefs == null) {
+            count++
+            fragManager.beginTransaction().replace(R.id.fragment_frame, PickEventTypeFragment(), null).commit()
+        } else {
+            Util.setPrefInt(act, AppContract.PREF_FIRST_OPEN_KEY, 1)
+            startActivity(intentFor<SetupProfileActivity>())
+            finish()
+        }
     }
 
     override fun eventsPickDone(selected: Set<String>) {
         Util.setPrefStringSet(act, AppContract.PREF_EVENT_TYPES_KEY, selected)
+        user?.let { fbManager.updateProfileField(AppContract.FIREBASE_USER_PREFERENCES, selected.toList(), it) }
         Log.d(TAG, "EventSet $selected")
         Util.setPrefInt(act, AppContract.PREF_FIRST_OPEN_KEY, 1)
         startActivity(intentFor<SetupProfileActivity>())
@@ -49,11 +73,15 @@ PickEventTypeFragment.PickEventTypeListener{
         super.onStart()
         if(FirebaseManager.isSignedIn()){
             Log.d(TAG, "user is signed in")
+            user = FirebaseManager.getUser()
+            observeUser()
         } else{
             Util.startSignInActivity(act)
         }
-        count++
-        fragManager.beginTransaction().replace(R.id.fragment_frame, PickUniversityFragment(), null).commit()
+        if(country == null || university == null) {
+            count++
+            fragManager.beginTransaction().replace(R.id.fragment_frame, PickUniversityFragment(), null).commit()
+        }
     }
 
     override fun onBackPressed() {
@@ -81,6 +109,16 @@ PickEventTypeFragment.PickEventTypeListener{
             } else {
                Log.d(TAG, "Sign in failed")
             }
+        }
+    }
+
+    private fun observeUser() {
+        user?.let {
+            viewModel.getProfileInfo(it)?.observe(this, Observer {
+                country = it?.get(AppContract.FIREBASE_USER_COUNTRY) as String?
+                university = it?.get(AppContract.FIREBASE_USER_UNIVERSITY) as String?
+                prefs = it?.get(AppContract.FIREBASE_USER_PREFERENCES) as List<String>?
+            })
         }
     }
 }
--- app/src/main/java/toluog/campusbash/view/PickEventTypeFragment.kt
+++ app/src/main/java/toluog/campusbash/view/PickEventTypeFragment.kt
@@ -1,5 +1,6 @@
 package toluog.campusbash.view
 
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.res.AssetManager
 import android.graphics.Typeface
@@ -14,28 +15,20 @@ import com.igalata.bubblepicker.BubblePickerListener
 import com.igalata.bubblepicker.adapter.BubblePickerAdapter
 import com.igalata.bubblepicker.model.BubbleGradient
 import com.igalata.bubblepicker.model.PickerItem
-import com.igalata.bubblepicker.rendering.BubblePicker
 import kotlinx.android.synthetic.main.pick_event_type_fragment_layout.*
-import kotlinx.android.synthetic.main.pick_event_type_fragment_layout.view.*
-import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.design.snackbar
 import toluog.campusbash.R
-import toluog.campusbash.R.array.colors
 import java.lang.ClassCastException
 
 /**
  * Created by oguns on 12/28/2017.
  */
-class PickEventTypeFragment(): Fragment() {
-
-    interface PickEventTypeListener {
-        fun eventsPickDone(selected: Set<String>)
-    }
+class PickEventTypeFragment: Fragment() {
 
     private var callback: PickEventTypeListener? = null
     private val TAG = PickEventTypeFragment::class.java.simpleName
-   private lateinit var rootView: View
-    lateinit var evetTypes: Array<String>
+    private lateinit var rootView: View
+    lateinit var eventTypes: Array<String>
     val selectedTypes = HashSet<String>()
 
     private lateinit var boldTypeface: Typeface
@@ -61,14 +54,16 @@ class PickEventTypeFragment(): Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        evetTypes = resources.getStringArray(R.array.party_types)
+        eventTypes = resources.getStringArray(R.array.party_types)
+
+        updateUi()
+
         next_button.setOnClickListener {
             if(selectedTypes.size < 2){
-                view?.context?.getString(R.string.items_at_least_two)?.let { x -> snackbar(container, x) }
+                view.context?.getString(R.string.items_at_least_two)?.let { x -> snackbar(container, x) }
             }
             else callback?.eventsPickDone(selectedTypes)
         }
-        updateUi()
     }
 
     override fun onResume() {
@@ -94,11 +89,11 @@ class PickEventTypeFragment(): Fragment() {
         picker.centerImmediately = true
         picker.bubbleSize = 20
         picker.adapter = object : BubblePickerAdapter {
-            override val totalCount = evetTypes.size
+            override val totalCount = eventTypes.size
             override fun getItem(position: Int): PickerItem {
                 return PickerItem().apply {
                     val colors = rootView.context.resources.obtainTypedArray(R.array.colors)
-                    title = evetTypes[position]
+                    title = eventTypes[position]
                     gradient = BubbleGradient(colors.getColor((position * 2) % 8, 0),
                             colors.getColor((position * 2) % 8 + 1, 0), BubbleGradient.VERTICAL)
                     typeface = mediumTypeface
@@ -125,5 +120,9 @@ class PickEventTypeFragment(): Fragment() {
                 }
             }
         }
+    }
+
+    interface PickEventTypeListener {
+        fun eventsPickDone(selected: Set<String>)
     }
 }
