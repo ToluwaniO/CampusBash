8e90098a5b953a9300e7762fddc8d1be49b7ff7f
ToluwaniO <ogunsanyatoluwani@gmail.com>
2018-07-02 14:14:35 -0400

    keep network data classes

--- app/src/main/java/toluog/campusbash/data/network/StripeAccountBody.kt
+++ app/src/main/java/toluog/campusbash/data/network/StripeAccountBody.kt
@@ -1,6 +1,8 @@
 package toluog.campusbash.data.network
 
+import android.support.annotation.Keep
 import toluog.campusbash.BuildConfig
 
+@Keep
 data class StripeAccountBody(val uid: String, val email: String, val country: String,
                                   val debug: Boolean = BuildConfig.DEBUG)
