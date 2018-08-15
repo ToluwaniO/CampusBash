e3babbacc5e3567319d1bbd9d995db7a2a8b702b
ToluwaniO <ogunsanyatoluwani@gmail.com>
2018-08-15 17:09:31 -0400

    emojis

---
+++ app/src/main/java/toluog/campusbash/CampusBashApplication.kt
@@ -0,0 +1,36 @@
+package toluog.campusbash
+
+import android.app.Application
+import android.graphics.Color
+import android.support.text.emoji.EmojiCompat
+import android.support.text.emoji.FontRequestEmojiCompatConfig
+import android.support.v4.provider.FontRequest
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+
+class CampusBashApplication: Application() {
+    private val TAG = CampusBashApplication::class.java.simpleName
+    override fun onCreate() {
+        super.onCreate()
+
+        val config: EmojiCompat.Config
+        val fontRequest = FontRequest(
+                "com.google.android.gms.fonts",
+                "com.google.android.gms",
+                "Noto Color Emoji Compat",
+                R.array.com_google_android_gms_fonts_certs)
+        config = FontRequestEmojiCompatConfig(applicationContext, fontRequest)
+                .setReplaceAll(true)
+                .registerInitCallback(object : EmojiCompat.InitCallback() {
+                    override fun onInitialized() {
+                        Log.d(TAG, "EmojiCompat initialized")
+                    }
+
+                    override fun onFailed(throwable: Throwable?) {
+                        Log.e(TAG, "EmojiCompat initialization failed", throwable)
+                        throwable?.let { Crashlytics.logException(throwable) }
+                    }
+                })
+        EmojiCompat.init(config)
+    }
+}
